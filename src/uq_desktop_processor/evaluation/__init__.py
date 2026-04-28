fbf97b21236adc9b9d4438136d15dff13d1008b7
Antek <antek.nikolajdu@gmail.com>
2026-04-28 13:10:50 +0200

    ci: unblock mypy check and restore evaluation exports

--- src/uq_desktop_processor/evaluation/__init__.py
+++ src/uq_desktop_processor/evaluation/__init__.py
@@ -3,9 +3,13 @@ Evaluation module:   init  .
 """
 
 from .clip_common import print_results
+from .clip_evaluator import evaluate_images_with_clip
 from .clip_prefilter import prefilter_folder
+from .finetuned_evaluator import evaluate_images_with_finetuned
 
 __all__ = [
+    "evaluate_images_with_clip",
+    "evaluate_images_with_finetuned",
     "prefilter_folder",
     "print_results",
 ]
